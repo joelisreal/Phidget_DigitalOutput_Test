6c5924f9771f24390f1a7ffdca3cbe485b8e1c0c
joelisreal <joel.igberase@gmail.com>
2019-04-15 13:34:42 -0600

    Initial Commit

---
+++ Phidget_DigitalOutput_Test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Phidget_DigitalOutput_Test
+//
+//  Created by Joel Igberase on 2019-04-15.
+//  Copyright © 2019 Joel Igberase. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Phidget_DigitalOutput_Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Phidget_DigitalOutput_Test
+//
+//  Created by Joel Igberase on 2019-04-15.
+//  Copyright © 2019 Joel Igberase. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
